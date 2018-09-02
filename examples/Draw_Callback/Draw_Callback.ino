c84c40a1df04e5617a565328aac63f033597747b
derfaq <daguerref@hotmail.com>
2018-09-02 19:44:55 -0300

    first commit

---
+++ examples/Draw_Callback/Draw_Callback.ino
@@ -0,0 +1,46 @@
+#include <RSD.h>
+
+RSD rsd;
+
+void setup() {
+  // put your setup code here, to run once:
+  rsd.begin(9,10,11,COMMON_ANODE);
+  rsd.attachDraw(draw);
+}
+
+int incomingByte;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  rsd.update();
+  
+  //Adjustment of frequency
+  int thick = map(analogRead(A0),0,1024,rsd.getLowerTick(),rsd.getHigherTick());
+  rsd.setTick(thick);
+  int fine = map(analogRead(A1),0,1024,rsd.getLowerLastTick(),rsd.getHigherLastTick());
+  rsd.setLastTick(fine);
+
+  //Adjustment of position screen
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+    // if it's a capital A, bring forward screen:
+    if (incomingByte == 'A') {
+      rsd.changePos(true);
+    }
+    // if it's an S, bring backward screen:
+    if (incomingByte == 'S') {
+      rsd.changePos(false);
+    }
+  }
+}
+
+//This function will be called in the start of every new frame
+void draw(){
+  //Test Screen for the Rolling Shutter Display
+  for(int i = 0; i < WIDTH; i++) {
+    colour color = ( i * 8 ) / WIDTH;
+    rsd.putBar(color,i);
+  }
+}
+
---
+++ examples/Test_Screen/Test_Screen.ino
@@ -0,0 +1,23 @@
+#include <RSD.h>
+
+RSD rsd;
+
+void setup() {
+  // put your setup code here, to run once:
+  rsd.begin(9,10,11,COMMON_ANODE);
+
+  //Test Screen for the Rolling Shutter Display
+  rsd.beginDraw();
+  
+  for(int i = 0; i < WIDTH; i++) {
+    colour color = ( i * 8 ) / WIDTH;
+    rsd.putBar(color,i);
+  }
+  
+  rsd.endDraw();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
