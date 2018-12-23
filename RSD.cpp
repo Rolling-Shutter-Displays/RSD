451b318d072f2820070275d3b5967958770d674c
derfaq <daguerref@hotmail.com>
2018-12-23 11:49:45 -0300

    Major changes

--- RSD.cpp
+++ RSD.cpp
@@ -36,27 +36,29 @@ static volatile uint16_t tick = TICK;
 static volatile uint16_t last = TICK;
 
 //"Video" memory for each channel (df for dataFrame)
-static volatile uint8_t dfR[BWIDTH];
-static volatile uint8_t dfG[BWIDTH];
-static volatile uint8_t dfB[BWIDTH];
+static volatile uint8_t dfR[ BWIDTH ];
+static volatile uint8_t dfG[ BWIDTH ];
+static volatile uint8_t dfB[ BWIDTH ];
 
 // Some kind of doble buffer video memory (db for dataBuffer or doubleBuffer),
 // it's *relative* safe to write here to be truly safe we need to implement some sort of signaling
-uint8_t dbR[BWIDTH];
-uint8_t dbG[BWIDTH];
-uint8_t dbB[BWIDTH];
+uint8_t dbR[ BWIDTH ];
+uint8_t dbG[ BWIDTH ];
+uint8_t dbB[ BWIDTH ];
 
 //Signalig variable
 		/* 0: Busy, not copy, from scketch to library, processing over dataBuffers
 		   1: Ready for copy, from scketch to library, end of processing
 		   2: Beging of frame, from library to scketch, ready for process, quick go! */
-volatile uint8_t frameStatus = 0; 
+volatile uint8_t frameStatus = 1; 
+volatile uint32_t frameCount = 0;
+volatile uint16_t frameLost = 0;
 
 //This variable will hold the function acting as event source.
 static callbackFunction _draw;
 
 //Interrupt rutine, all the magic happens here
-ISR(TIMER1_COMPA_vect) {
+ISR( TIMER1_COMPA_vect ) {
 	
 	//Calculate the bit position in the memory that now we need to looking for
 	//May be this could work better using pointers, isn't?
@@ -68,44 +70,44 @@ ISR(TIMER1_COMPA_vect) {
   	
   	if (led_type) { //If led_type = 1 = COMMON_ANODE
 
-  		if ( bitmask & dfR[idx] ) { 	//If we have a one (on) in the memory
-  		    *pinR_port &= ~(pinR_mask); //We need to turn off the pin (sink current)
+  		if ( bitmask & dfR[ idx ] ) { 	//If we have a one (on) in the memory
+  		    *pinR_port &= ~( pinR_mask ); //We need to turn off the pin (sink current)
   		} else { 						//If we have a zero (off) in the memory
     		*pinR_port |= pinR_mask; 	//We need to turn on the pin (not current)
   		}
 
   		//Same for the rest of the channels, green and blue
-  		if ( bitmask & dfG[idx] ) {
-    		*pinG_port &= ~(pinG_mask);
+  		if ( bitmask & dfG[ idx ] ) {
+    		*pinG_port &= ~( pinG_mask );
   		} else {
     		*pinG_port |= pinG_mask; 
   		}
 
-  		if ( bitmask & dfB[idx] ) {
-    		*pinB_port &= ~(pinB_mask);
+  		if ( bitmask & dfB[ idx ] ) {
+    		*pinB_port &= ~( pinB_mask );
   		} else {
     		*pinB_port |= pinB_mask; 
   		}
   
 	} else { // Else if led_type = 0 = COMMON_CATHODE
 
-  		if ( bitmask & dfR[idx] ) { 	//If we have a one (on) in the memory
+  		if ( bitmask & dfR[ idx ] ) { 	//If we have a one (on) in the memory
   			*pinR_port |= pinR_mask;    //We need to turn on the pin (source current)
   		} else { 						//If we have a zero (off) in the memory
-    		*pinR_port &= ~(pinR_mask);	//We need to turn off the pin (not current)
+    		*pinR_port &= ~( pinR_mask );	//We need to turn off the pin (not current)
   		}
 
   		//Same for the rest of the channels, green and blue
-  		if ( bitmask & dfG[idx] ) {
+  		if ( bitmask & dfG[ idx ] ) {
   			*pinG_port |= pinG_mask;    
   		} else { 						
-    		*pinG_port &= ~(pinG_mask);	
+    		*pinG_port &= ~( pinG_mask );	
   		}
 
-  		if ( bitmask & dfB[idx] ) { 	
+  		if ( bitmask & dfB[ idx ] ) { 	
   			*pinB_port |= pinB_mask;    
   		} else { 						
-    		*pinB_port &= ~(pinB_mask);	
+    		*pinB_port &= ~( pinB_mask );	
   		}
   	}
   	
@@ -113,34 +115,46 @@ ISR(TIMER1_COMPA_vect) {
   	pos++;
 
   	//Special moments
-  	if (pos == WIDTH-1) { 
-    	OCR1A = last; //fine correction of frequency
+  	if ( pos ==  WIDTH - 2 ) { 
+    	OCR1A = last; //fine correction of frequency <<-- this method can't be for general frequency, luckily work's really fine in 30 Hz
+  	}
+  	
+  	//Interesting 
+  	if ( pos == WIDTH - 1 ) { 
+    	OCR1A = tick; //backwards period
   	}
 
   	//If the position is the first
-  	if (pos >= WIDTH) { 
-    	if(frameStatus) { //And the buffer is ready to copy
-      		for(uint8_t i = 0; i < BWIDTH; i++) { //Copy the buffer to "video" memory
+  	if ( pos == WIDTH ) { 
+        
+        // Restore the tick period (thick tuning), don't worry OCR1A is intrisic double buffer too
+    	// it will be updated in the next frame
+    	//OCR1A = tick;
+        
+    	pos = 0;
+        frameCount++;
+        
+        if( frameStatus == 1 ) { //And the buffer is ready to copy
+      		for( uint8_t i = 0 ; i < BWIDTH ; i++ ) { //Copy the buffer to "video" memory
         		dfR[i] = dbR[i]; 
         		dfG[i] = dbG[i]; 
         		dfB[i] = dbB[i]; 
-      		}
+            }
+            
+            //Tells to scketch that is a good moment for begin a process over buffers //This is the right place?
+            frameStatus = 2;
+        } else {
+            frameLost++;
+        }
+    	// else: don't copy. It would nice to give a return, what would allow us to count the
+    	// lost frames . 10/12/18 Okey.
     	
-    	} // else: don't copy. It would nice to give a return, what would allow us to count the
-    	  // lost frames
-
-    	// Restore the tick period (thick tuning), don't worry OCR1A is intrisic double buffer too
-    	// it will be updated in the next frame
-    	OCR1A = tick;
-    	pos = 0;
-    	//Tells to scketch that is a good moment for begin a process over buffers
-    	frameStatus = 2;
   	}
   
 }
 
 
-void RSD::begin(uint8_t pinR, uint8_t pinG, uint8_t pinB, common_type commonType) {
+void RSD::begin( uint8_t pinR , uint8_t pinG , uint8_t pinB , common_type commonType ) {
 	//Save the numers pins
 	pin_R = pinR;
 	pin_G = pinG;
@@ -182,133 +196,6 @@ void RSD::initTimer1(){
 	TIMSK1 |= (1<<OCIE1A); //Set Output Compare A Match Interrupt Enable
 }
 
-void RSD::putBar(colour Color, uint16_t _pos) { //Stupid way but, whaterever
-												//Try rearranging the colors order
-	switch(Color) {
-		case BLACK:
-			clearBar(dbR,_pos);
-			clearBar(dbG,_pos);
-			clearBar(dbB,_pos);
-			break;
-		case RED:
-			putBar(dbR,_pos);
-			clearBar(dbG,_pos);
-			clearBar(dbB,_pos);
-			break;
-		case GREEN:
-			clearBar(dbR,_pos);
-			putBar(dbG,_pos);
-			clearBar(dbB,_pos);
-			break;
-		case BLUE:
-			clearBar(dbR,_pos);
-			clearBar(dbG,_pos);
-			putBar(dbB,_pos);
-			break;
-		case YELLOW:
-			putBar(dbR,_pos);
-			putBar(dbG,_pos);
-			clearBar(dbB,_pos);
-			break;
-		case CYAN:
-			clearBar(dbR,_pos);
-			putBar(dbG,_pos);
-			putBar(dbB,_pos);
-			break;
-		case MAGENTA:
-			putBar(dbR,_pos);
-			clearBar(dbG,_pos);
-			putBar(dbB,_pos);
-			break;
-		case WHITE:
-			putBar(dbR,_pos);
-			putBar(dbG,_pos);
-			putBar(dbB,_pos);
-			break;
-		default: break;
-	}
-}
-
-colour RSD::getBar(uint16_t _pos) { //Stupid way but, whaterever
-									//Try rearranging the colors order
-	if(getChannel(dbR,_pos)) {
-		
-		if(getChannel(dbG,_pos)) {
-			if (getChannel(dbB,_pos)) {
-				return WHITE;
-			} else {
-				return YELLOW;
-			}
-		} else {
-			if (getChannel(dbB,_pos)) {
-				return MAGENTA;
-			} else {
-				return RED;
-			}
-		}
-
-	} else if(getChannel(dbG,_pos)) {
-		
-		if(getChannel(dbB,_pos)) {
-			return CYAN;
-		} else {
-			return GREEN;
-		}
-
-	} else if (getChannel(dbB,_pos)) {
-			return BLUE;
-		} else {
-			return BLACK;
-	}
-}
-
-
-void RSD::fillScreen(colour Color) {
-	switch(Color) {
-		case BLACK:
-			clearChannel(dbR);
-			clearChannel(dbG);
-			clearChannel(dbB);
-			break;
-		case RED:
-			fillChannel(dbR);
-			clearChannel(dbG);
-			clearChannel(dbB);
-			break;
-		case GREEN:
-			clearChannel(dbR);
-			fillChannel(dbG);
-			clearChannel(dbB);
-			break;
-		case BLUE:
-			clearChannel(dbR);
-			clearChannel(dbG);
-			fillChannel(dbB);
-			break;
-		case YELLOW:
-			fillChannel(dbR);
-			fillChannel(dbG);
-			clearChannel(dbB);
-			break;
-		case CYAN:
-			clearChannel(dbR);
-			fillChannel(dbG);
-			fillChannel(dbB);
-			break;
-		case MAGENTA:
-			fillChannel(dbR);
-			clearChannel(dbG);
-			fillChannel(dbB);
-			break;
-		case WHITE:
-			fillChannel(dbR);
-			fillChannel(dbG);
-			fillChannel(dbB);
-			break;
-		default: break;
-	}
-}
-
 //Tunnig functions
 
 uint16_t RSD::getTick() {
@@ -316,11 +203,11 @@ uint16_t RSD::getTick() {
 }
 
 uint16_t RSD::getLowerTick() {
-	return	F_CPU/((F_CAM + 1)*WIDTH);
+	return	F_CPU / ( ( F_CAM + 1 ) * WIDTH );
 }
 
 uint16_t RSD::getHigherTick() {
-	return	F_CPU/((F_CAM - 1)*WIDTH);
+	return	F_CPU / ( (F_CAM - 1) * WIDTH );
 }
 
 uint16_t RSD::getLastTick() {
@@ -337,7 +224,7 @@ uint16_t RSD::getHigherLastTick() {
 
 bool RSD::setTick(int _tick) {
 
-	if( (_tick > getLowerTick()) &&	(_tick < getHigherTick()) ) {
+	if( ( _tick > getLowerTick() ) &&	(  _tick < getHigherTick() ) ) {
 		tick = _tick;
 		return true;
 	} else {
@@ -345,9 +232,9 @@ bool RSD::setTick(int _tick) {
 	}
 }
 
-bool RSD::setLastTick(int _tick) {
+bool RSD::setLastTick( int _tick ) {
 	
-	if( (_tick  > getLowerLastTick()) && (_tick  < getHigherLastTick()) ) {
+	if( ( _tick  > getLowerLastTick() ) && ( _tick  < getHigherLastTick() ) ) {
 			last = _tick;
 			return true;
 		} else {
@@ -356,20 +243,20 @@ bool RSD::setLastTick(int _tick) {
 }
 
 uint32_t RSD::getPeriod() {
-	return (uint32_t)tick*(WIDTH - 1) + last;
+	return (uint32_t) tick * (WIDTH - 1) + last;
 }
 
 float RSD::getFrequency() {
-	return (float)F_CPU/getPeriod();
+	return (float) F_CPU / getPeriod();
 }
 
 void RSD::changePos(bool direction) {
-	if (direction) { //If direction is forward
-		if (pos < WIDTH) {
+	if ( direction ) { //If direction is forward
+		if ( pos < WIDTH ) {
 			pos++;
 		} 
 	} else { //If direction is backwards
-		if (pos > 0) {
+		if ( pos > 0 ) {
 			pos--;
 		}
 	}
@@ -377,15 +264,160 @@ void RSD::changePos(bool direction) {
 }
 
 void RSD::update() {
-	if (frameStatus==2) {
-		beginDraw();
+	if ( frameStatus == 2 ) {
+		frameStatus = 0;
 		_draw();
-		endDraw();
+		frameStatus = 1;
 	}
 }
 
 //Callback function, let's draw!
-void RSD::attachDraw(callbackFunction newFunction)
-{
+void RSD::attachDraw( callbackFunction newFunction ) {
 	_draw = newFunction;
 } // attachDraw
+
+
+// Graphic screen primitives
+
+// Drawing primitives
+
+void RSD::putLine( colour c , uint16_t _pos ) { //Stupid way but, whaterever
+												//Try rearranging the colors order
+	switch( c ) {
+		case BLACK:
+			clearLine( dbR , _pos );
+			clearLine( dbG , _pos );
+			clearLine( dbB , _pos );
+			break;
+		case RED:
+			putLine( dbR , _pos );
+			clearLine( dbG , _pos );
+			clearLine( dbB , _pos );
+			break;
+		case GREEN:
+			clearLine( dbR , _pos );
+			putLine( dbG , _pos );
+			clearLine( dbB , _pos );
+			break;
+		case BLUE:
+			clearLine( dbR , _pos );
+			clearLine( dbG , _pos );
+			putLine( dbB , _pos );
+			break;
+		case YELLOW:
+			putLine( dbR , _pos );
+			putLine( dbG , _pos );
+			clearLine( dbB , _pos );
+			break;
+		case CYAN:
+			clearLine( dbR , _pos );
+			putLine( dbG , _pos );
+			putLine( dbB , _pos );
+			break;
+		case MAGENTA:
+			putLine( dbR , _pos );
+			clearLine( dbG , _pos );
+			putLine( dbB , _pos );
+			break;
+		case WHITE:
+			putLine( dbR , _pos );
+			putLine( dbG , _pos );
+			putLine( dbB , _pos );
+			break;
+		default: break;
+	}
+
+    
+}
+
+void RSD::fillRange( colour c , uint16_t begin , uint16_t end ) {
+    if ( !( begin >= WIDTH ) || ( end >= WIDTH ) || ( begin > end ) ) { 
+        while ( end > begin ) {
+            RSD::putLine( c , end );
+            end--;
+        }
+        RSD::putLine( c , begin );
+    }
+}
+
+void RSD::fillScreen( colour c = WHITE ) {
+	
+    switch( c ) {
+		case BLACK:
+			clearChannel( dbR );
+			clearChannel( dbG );
+			clearChannel( dbB );
+			break;
+        case RED:
+			fillChannel( dbR );
+			clearChannel( dbG );
+			clearChannel( dbB );
+			break;
+        case GREEN:
+			clearChannel( dbR );
+			fillChannel( dbG );
+			clearChannel( dbB );
+			break;
+		case BLUE:
+			clearChannel( dbR );
+			clearChannel( dbG );
+			fillChannel( dbB );
+			break;
+		case YELLOW:
+			fillChannel( dbR );
+			fillChannel( dbG );
+			clearChannel( dbB );
+			break;
+		case CYAN:
+			clearChannel( dbR );
+			fillChannel( dbG );
+			fillChannel( dbB );
+			break;
+		case MAGENTA:
+			fillChannel( dbR );
+			clearChannel( dbG );
+			fillChannel( dbB );
+			break;
+		case WHITE:
+			fillChannel( dbR );
+			fillChannel( dbG );
+			fillChannel( dbB );
+			break;
+		default: break;
+	}
+}
+
+// Get functions
+
+colour RSD::getLine( uint16_t _pos ) { //Stupid way but, whaterever
+									//Try rearranging the colors order
+	if(getChannel(dbR,_pos)) {
+		
+		if(getChannel( dbG , _pos ) ) {
+			if ( getChannel( dbB , _pos ) ) {
+				return WHITE;
+			} else {
+				return YELLOW;
+			}
+		} else {
+			if ( getChannel( dbB , _pos ) ) {
+				return MAGENTA;
+			} else {
+				return RED;
+			}
+		}
+
+	} else if( getChannel( dbG, _pos ) ) {
+		
+		if( getChannel( dbB, _pos) ) {
+			return CYAN;
+		} else {
+			return GREEN;
+		}
+
+	} else if ( getChannel( dbB , _pos ) ) {
+			return BLUE;
+		} else {
+			return BLACK;
+	}
+}
--- RSD.h
+++ RSD.h
@@ -25,118 +25,55 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 #define BWIDTH 32 //number of bytes
-#define WIDTH (BWIDTH*8) //number of lines (bits)
+#define WIDTH ( BWIDTH * 8 ) //number of lines (bits)
 
 
 #define F_CAM 30 //Assumes that the frequency of the camera is 30 hz
-#define TICK F_CPU/(F_CAM*WIDTH) //initial frequency
-
-
-typedef enum 
-{
-  // Used to indicate the RGB LED is a Common Cathode LED
-  COMMON_CATHODE = 0,
-  // Used to indicate the RGB LED is a Common Anode LED
-  COMMON_ANODE   = 1,
-} common_type; //From https://github.com/BretStateham/RGBLED/blob/master/RGBLED.h
-			   //See too https://github.com/VaSe7u/RGBLED/blob/master/src/RGBLED.hpp
-
-typedef enum 
-{
-  BLACK,RED,GREEN,BLUE,YELLOW,CYAN,MAGENTA,WHITE
-} colour;
-
-// ----- Callback function types -----
-
-extern "C" {
-  typedef void (*callbackFunction)(void);
-}
+#define TICK F_CPU / ( F_CAM * WIDTH ) //initial frequency
 
 //Some kind of doble buffer (db) video memory, it's *relative* safe to write here
 //to be truly safe we need to implement some sort of signaling
-extern uint8_t dbR[BWIDTH];
-extern uint8_t dbG[BWIDTH];
-extern uint8_t dbB[BWIDTH];
+extern uint8_t dbR[ BWIDTH ];
+extern uint8_t dbG[ BWIDTH ];
+extern uint8_t dbB[ BWIDTH ];
+
 //Signalig variable
 		/* 0: Busy, not copy, from scketch to library, processing over dataBuffers
 		   1: Ready for copy, from scketch to library, end of processing
 		   2: Beging of frame, from library to scketch, ready for process, quick go! */
-extern  volatile uint8_t  frameStatus; 
+extern volatile uint8_t frameStatus;
+extern volatile uint32_t frameCount;
+extern volatile uint16_t frameLost; 
+
+//Callback function types
+extern "C" {
+  typedef void ( *callbackFunction )( void );
+}
+
+typedef enum { COMMON_CATHODE = 0, COMMON_ANODE = 1 } common_type;
+
+typedef enum { BLACK , RED , GREEN , BLUE , YELLOW , CYAN , MAGENTA , WHITE } colour;
+
 
 class RSD {
 	
 	public:
-		//Needs to be called in the setup()
-		static void begin(uint8_t pinR, uint8_t pinG, uint8_t pinB, common_type commonType = COMMON_ANODE);
+		
+        // Begin function. Needs to be called in the setup()
+        
+		static void begin( uint8_t pinR , uint8_t pinG , uint8_t pinB , common_type commonType = COMMON_ANODE );
 
-		//Update
+		// Update function. Needs to be called in the loop() 
+        
 		static void update();
+		
+		// Callback function attachment. Needs to be called in the setup(), and update() running in the loop()
+		
+		static void attachDraw( callbackFunction newFunction );
 
-		//Callback function
-		static void attachDraw(callbackFunction newFunction);
-
-		static inline void beginDraw() {
-			frameStatus = 0;
-		}
-
-		static inline void endDraw() {
-			frameStatus = 1;
-		}
-
-
-		//unsafe (not boundary checks) but eficient methods to draw a bar
-		static inline void putBar(uint8_t *channel, uint16_t _pos) {
-			channel[_pos / 8] |= (1 << _pos % 8);
-		}
-
-		static void putBar(colour Color, uint16_t _pos);
-
-		static inline void clearBar(uint8_t *channel, uint16_t _pos) {
-			channel[_pos / 8] &= ~(1 << _pos % 8);
-		}
-
-		static inline void clearBar(uint16_t _pos) {
-			clearBar(dbR, _pos);
-			clearBar(dbG, _pos);
-			clearBar(dbB, _pos);
-
-		}
-
-		static inline bool getChannel(uint8_t *channel, uint16_t _pos) {
-			return channel[_pos / 8]&(1 << _pos % 8) ? true : false;
-		}
-
-		static colour getBar(uint16_t _pos);
-
-
-		static inline clearChannel(uint8_t *channel) {
-			for(uint8_t i = 0; i < BWIDTH; i++) {
-				channel[i] = 0;
-			}
-		}
-
-		static inline fillChannel(uint8_t *channel) {
-			for(uint8_t i = 0; i < BWIDTH; i++) {
-				channel[i] = 0xff;
-			}	
-		}
-
-		static inline clearScreen() {
-			clearChannel(dbR);
-			clearChannel(dbG);
-			clearChannel(dbB);
-		}
-
-		static inline fillScreen() {
-			fillChannel(dbR);
-			fillChannel(dbG);
-			fillChannel(dbB);
-		}
-
-		static void fillScreen(colour Color);
-
-		//Tunning functions
-		static uint16_t getTick();
+		// Tunning functions
+		
+        static uint16_t getTick();
 		
 		static uint16_t getLowerTick();
 
@@ -157,13 +94,103 @@ class RSD {
 		static float getFrequency();
 
 		static void changePos(bool direction);
+        
+        
+        // Graphics primitives
+        
+        // Drawing methods
+        
+        // Unsafe and fast methods
+        
+		static inline void putLine( uint8_t *channel , uint16_t _pos ) {
+			channel[ _pos / 8 ] |= ( 1 << _pos % 8 );
+		}
 
+		static void putLine( colour c , uint16_t _pos );
+        
+        // Safe methods
+        
+        static inline void fillRange( uint8_t *channel , uint16_t begin , uint16_t end ) {
+            if ( !( ( begin >= WIDTH ) || ( end >= WIDTH ) || ( begin > end ) ) ) { 
+                while ( end > begin ) {
+                    RSD::putLine( channel , end );
+                    end--;
+                }
+                RSD::putLine( channel , begin );
+            }
+        }
+    
+        static void fillRange( colour c , uint16_t begin , uint16_t end );
+        
+        
+        static inline void fillChannel( uint8_t *channel ) {
+			for( uint8_t i = 0 ; i < BWIDTH ; i++ ) {
+				channel[ i ] = 0xff;
+			}	
+		}
+
+		static void fillScreen( colour c );
+        
+        // Erasing methods
+        
+        // Unsafe methods
+        
+		static inline void clearLine( uint8_t *channel , uint16_t _pos) {
+			channel[ _pos / 8 ] &= ~( 1 << _pos % 8 );
+		}
+		
+		static inline void clearLine( uint16_t _pos ) {
+			clearLine( dbR, _pos );
+			clearLine( dbG, _pos );
+			clearLine( dbB, _pos );
+		}
+		
+		// Safe methods
+		
+		static inline void clearRange( uint8_t *channel , uint16_t begin , uint16_t end ) {
+            if ( !( begin >= WIDTH ) || ( end >= WIDTH ) || ( begin > end ) ) { 
+                while ( end > begin ) {
+                    RSD::clearLine( channel , end );
+                    end--;
+                }
+                RSD::clearLine( channel , begin );
+            }
+        }
+        
+        static inline void clearRange( uint16_t begin , uint16_t end ) {
+            clearRange( dbR, begin , end );
+			clearRange( dbG, begin , end );
+			clearRange( dbB, begin , end );
+        }
+            
+		
+		static inline void clearChannel( uint8_t *channel ) {
+			for(uint8_t i = 0; i < BWIDTH; i++) {
+				channel[ i ] = 0;
+			}
+        }
+        
+        static inline void clearScreen() {
+			clearChannel( dbR );
+			clearChannel( dbG );
+			clearChannel( dbB );
+		}
+
+		// Get methods
+		
+		// Unsafe methods
+		
+		static inline bool getChannel( uint8_t *channel , uint16_t _pos ) {
+			return channel[ _pos / 8 ] & ( 1 << _pos % 8 ) ? true : false;
+		}
+
+		static colour getLine( uint16_t _pos );
+        
 	private:
-
+        
 		//Timer one initialization method
+        
 		static void initTimer1();
-
-
 };
 
 #endif
