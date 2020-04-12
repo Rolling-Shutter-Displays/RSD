c0b2f5a0046f289fcedfc88e51257ee9bd586e7d
derfaq <daguerref@hotmail.com>
2020-04-12 19:25:46 -0300

    wip

--- examples/TestScreen_RGB_LED/TestScreen_RGB_LED.ino
+++ examples/TestScreen_RGB_LED/TestScreen_RGB_LED.ino
@@ -1,15 +1,15 @@
 /*
-  TestScreen_One_LED.ino
+  TestScreen_RGB_LED.ino
 
   This is a example sketch to show how to use the RSD library to make a Rolling Shutter Display
-  using just one led.
+  using a RGB led.
 
-  For tuning, open the serial monitor, type the frequency and hit enter. The data shows the
-  current frequency of the RSD.
+  Use the potentiometer to tuning the RSD in the range of +-0.5 Hz.
+  The data in the serial monitor shows the current frequency of the RSD.
 
   For further explanation see: https://github.com/Rolling-Shutter-Displays/RSD
   
-  created 24 Feb 2019
+  created 5 Mar 2019
   modified 12 Apr 2020
   by derfaq
   
@@ -17,24 +17,29 @@
 */
 
 #include <RSD.h>
+#include <Channel.h>
+#include <Screen.h>
+
 RSD rsd;
+#define BWIDTH 32
+#define WIDTH ((BWIDTH*8)-1)
 
-#define BWIDTH 32             //Byte resolution
-#define WIDTH (BWIDTH*8 - 1)  //Line resolution
+Channel red( 11 , COMMON_ANODE , BWIDTH );
+Channel green( 3 , COMMON_ANODE , BWIDTH );
+Channel blue( 6 , COMMON_ANODE , BWIDTH );
 
-#define LED_PIN 8
-
-//Channel( pin , led mode , byte resolution )
-Channel white( LED_PIN , COMMON_CATHODE , BWIDTH );
+Screen display( &red , &green , &blue );
 
 void setup() {
 
   //Setup of the RSD 
-  rsd.begin( 30 , BWIDTH );    //begin( frequency , byte resolution )
+  rsd.begin( 30 , BWIDTH );
   
-  rsd.attachChannel( &white ); //attachChannel( channel )
-  
-  rsd.attachDraw( draw );      //attachDraw( callback function )
+  rsd.attachChannel( &red );
+  rsd.attachChannel( &green );
+  rsd.attachChannel( &blue );
+
+  rsd.attachDraw( draw );
 
   Serial.begin( 9600 );
   
@@ -51,16 +56,16 @@ void loop() {
   */
 
   //Tuning: Analog way
-  /*
   float freq = 30.0 - ( ( (float)analogRead( A0 ) / 1023.0 ) - 0.5 );
   rsd.setFrequency( freq );
-  */
   
   //Tuning: Serial way
+  /*
   if ( Serial.available() ){
     float freq = Serial.parseFloat();
     if ( freq != 0 ) rsd.setFrequency( freq );
   }
+  */
   
   //Serial diagnosis
   Serial.print("@frsd: ");
@@ -76,22 +81,19 @@ void loop() {
 
 //Let's draw!
 void draw() {
-  
-  white.clear();
-  
-  white.fill( 0 , WIDTH/4 );
 
-  for( int i = 1 ; i < (WIDTH/4) ; i++ ) {
-    int val = (WIDTH/4)/i;
-    if ( i%2 ) {
-      white.clear( WIDTH/2 - val , WIDTH/2 + val );
-    } else {
-      white.fill( WIDTH/2 - val , WIDTH/2 + val );
-    }
+  display.clear();
+  
+  //Standarized order of the SMPTE/EBU color bar image : https://en.wikipedia.org/wiki/SMPTE_color_bars
+  //from left to right, the colors are white, yellow, cyan, green, magenta, red, blue and black
+  for( int i = 0 ; i <= WIDTH ; i++ ) {
+    colour c = ( i * 8 ) / WIDTH;
+    display.line( i , c );
   }
 
-  for( int i = 0 ; i < WIDTH/4 ; i++ ) {
-    if( i%2 ) white.line( WIDTH - i );
+  //Grid resolution
+  for( int i = 0 ; i < BWIDTH/2 ; i++ ) {
+    if( 1 - i%2 ) display.line( i , WHITE );
   }
-  
+
 }
