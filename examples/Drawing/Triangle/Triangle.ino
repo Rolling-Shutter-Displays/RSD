068482acf8b8adedd12d1270fd349e5a3fc8e32b
derfaq <daguerref@hotmail.com>
2020-04-13 20:52:48 -0300

    wip

--- examples/Drawing/Triangle/Triangle.ino
+++ examples/Drawing/Triangle/Triangle.ino
@@ -4,11 +4,12 @@
   This is part of a series of sketch examples to show the primitives for drawing 
   of the RSD library.
 
-  Use the potentiometer to tuning the RSD in the range of +-1hz.
+  Use the potentiometer to tuning the RSD in the range of +-0.5 Hz.
 
   *For full explanation see: https://github.com/Rolling-Shutter-Displays/RSD
   
   created 9 Mar 2019
+  modified 13 Apr 2020
   by derfaq
   
   This example code is in the public domain.
@@ -29,6 +30,7 @@ Channel blue( 6 , COMMON_ANODE , BWIDTH );
 Screen display( &red , &green , &blue );
 
 void setup() {
+  
   //Setup of the RSD 
   rsd.begin( 30 , BWIDTH );
   
@@ -37,27 +39,37 @@ void setup() {
   rsd.attachChannel( &blue );
 
   rsd.attachDraw( draw );
+  
 }
 
 void loop() {
+  
   //Run the engine
   rsd.update();
   
   //Tuning: Analog way
-  int tick = map( analogRead(A0) , 0 , 1023 , rsd.getLowerTick() , rsd.getHigherTick() );
-  rsd.setTick( tick );
-  rsd.setFine( tick );
+  float freq = 30.0 - ( ( (float)analogRead( A0 ) / 1023.0 ) - 0.5 );
+  rsd.setFrequency( freq );
+  
 }
+
+// Complementary colours
+colour palette[2] = { RED , CYAN };
+// colour palette[2] = { GREEN , MAGENTA };
+// colour palette[2] = { BLUE , YELLOW };
 
 //Let's draw!
 void draw() {
-  display.fill( BLUE );
-  display.fill( WIDTH/2 - 10 , WIDTH/2 + 10 , RED );
-  triangle( 0  , WIDTH/2 , 10 , RED );
-  triangle( WIDTH , WIDTH/2 , 10 , RED );
+  
+  display.fill( palette[0] );
+  display.fill( WIDTH/2 - 10 , WIDTH/2 + 10 , palette[1] );
+  triangle( 0  , WIDTH/2 , 10 , palette[1] );
+  triangle( WIDTH , WIDTH/2 , 10 , palette[1] );
+  
 }
 
 void triangle( int begin , int end , int steps , colour c ) {
+  
   int interval;
   if( begin <= end ) {
     interval = ( end - begin ) / steps;
@@ -70,4 +82,5 @@ void triangle( int begin , int end , int steps , colour c ) {
       display.fill( begin - interval*i , begin - interval*i - map( i , 1 , steps, 0 , interval ) , c ); 
     }
   }
+  
 }
